b81d0badba32a9322c45b4ba2f7bd50daf2d7a67
Francisco Astorga <fastorga@nimok.com>
2021-09-29 23:10:55 -0300

    Added Input component
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import useFormulario from './hooks/useFormulario'
+import Input from './components/Input'
 
 function App() {
-  const [formulario, handleChange] = useFormulario({ name: ''})
+  const [formulario, handleChange] = useFormulario({ name: '', lastName: ''})
   
-  console.log(formulario.name)
+  console.log(formulario)
   return (
     <form>
-      <input 
-        name="name" 
-        placeholder="Nombre" 
-        value={formulario.name} 
+      <Input 
+        label='Nombre'
+        name='name'
+        placeholder='Nombre'
+        value={formulario.name}
         onChange={handleChange}
-        />
+      />
+      <Input 
+        label='Apellido'
+        name='lastName'
+        placeholder='Apellido'
+        value={formulario.lastName}
+        onChange={handleChange}
+      />
     </form>
   );
 }
