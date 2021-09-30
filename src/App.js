32a8ceb3c7b367f16cc69115d03dacf5fec60c45
Francisco Astorga <fastorga@nimok.com>
2021-09-30 00:33:20 -0300

    Added UserForm component and improved form handling to improve performance
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/App.js
+++ src/App.js
@@ -1,69 +1,35 @@
 import { useState } from 'react'
-import useFormulario from './hooks/useFormulario'
-import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
-import Button from './components/Button'
+import UserForm from './components/UserForm'
 
 function App() {
   const [usuarios, setUsuarios] = useState([])
-  const [formulario, handleChange, reset] = useFormulario(
-    { name: '', 
-    lastName: '', 
-    email: '' 
-  })
 
-  const submit = e => {
-    e.preventDefault()
+  const submit = usuario => {
     setUsuarios([
       ...usuarios,
-      formulario
+      usuario
     ])
-    reset()
   }
-  
+
   return (
-    <div style={{marginTop: '15%'}}>
+    <div style={{ marginTop: '15%' }}>
       <Container>
-      <Card>
-        <div style={{ padding: 20 }}>
-          <form onSubmit={submit}>
-            <Input
-              label='Nombre'
-              name='name'
-              placeholder='Nombre'
-              value={formulario.name}
-              onChange={handleChange}
-            />
-            <Input
-              label='Apellido'
-              name='lastName'
-              placeholder='Apellido'
-              value={formulario.lastName}
-              onChange={handleChange}
-            />
-            <Input
-              label='Correo'
-              name='email'
-              placeholder='Correo'
-              value={formulario.email}
-              onChange={handleChange}
-            />
-            <Button>Enviar</Button>
-          </form>
-        </div>
+        <Card>
+          <div style={{ padding: 20 }}>
+            <UserForm submit={submit} />
+          </div>
 
-      </Card>
-      <Card>
-        <ul>
-          {usuarios.map(x => <li key={x.email}>{`${x.name} ${x.lastName} ${x.email}`}</li>)}
-        </ul>
-      </Card>
-    </Container >
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map(x => 
+            <li key={x.email}>{`${x.name} ${x.lastName} ${x.email}`}</li>)}
+          </ul>
+        </Card>
+      </Container >
     </div>
-    
-
-
   );
 }
 
