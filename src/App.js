baf83ceea7d386b323c4f6216a6a77c6e2fa9237
Francisco Astorga <fastorga@nimok.com>
2021-09-29 23:22:15 -0300

    Added Card and Container components
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import useFormulario from './hooks/useFormulario'
 import Input from './components/Input'
+import Card from './components/Card'
+import Container from './components/Container'
 
 function App() {
-  const [formulario, handleChange] = useFormulario({ name: '', lastName: ''})
-  
+  const [formulario, handleChange] = useFormulario({ name: '', lastName: '' })
+
   console.log(formulario)
   return (
-    <form>
-      <Input 
-        label='Nombre'
-        name='name'
-        placeholder='Nombre'
-        value={formulario.name}
-        onChange={handleChange}
-      />
-      <Input 
-        label='Apellido'
-        name='lastName'
-        placeholder='Apellido'
-        value={formulario.lastName}
-        onChange={handleChange}
-      />
-    </form>
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label='Nombre'
+              name='name'
+              placeholder='Nombre'
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label='Apellido'
+              name='lastName'
+              placeholder='Apellido'
+              value={formulario.lastName}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+
+      </Card>
+    </Container >
+
+
   );
 }
 
